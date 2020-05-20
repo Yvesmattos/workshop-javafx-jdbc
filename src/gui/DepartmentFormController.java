9bc3b77b3d9d83c04a76f55bc3148076e87c9397
Yves Mattos <yvesmattos@gmail.com>
2020-05-20 19:36:12 -0300

    Saving a new Department

--- src/gui/DepartmentFormController.java
+++ src/gui/DepartmentFormController.java
@@ -3,62 +3,96 @@ package gui;
 import java.net.URL;
 import java.util.ResourceBundle;
 
+import db.DbException;
+import gui.util.Alerts;
 import gui.util.Constraints;
+import gui.util.Utils;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+import javafx.scene.control.Alert.AlertType;
 import model.entities.Department;
+import model.services.DepartmentService;
 
-public class DepartmentFormController implements Initializable{
-	
+public class DepartmentFormController implements Initializable {
+
 	private Department entity;
+	private DepartmentService service;
 
 	@FXML
 	private TextField txtId;
-	
+
 	@FXML
-	private TextField txtName; 
-	
+	private TextField txtName;
+
 	@FXML
-	private Label labelErroName;												
-	
+	private Label labelErroName;
+
 	@FXML
 	private Button btSave;
-	
+
 	@FXML
 	private Button btCancel;
-	
+
 	@FXML
-	private void onBtSaveAction() {
-		System.out.println("onBtSaveAction");
+	private void onBtSaveAction(ActionEvent event) {
+		if (entity == null) {
+			throw new IllegalStateException("Entidade estava nula");
+		}
+		if (service == null) {
+			throw new IllegalStateException("Serviço estava nula");
+		}
+		try {
+			entity = getFormData();
+			service.saveOrUpdate(entity);
+			Utils.currentStage(event).close();
+		} catch (DbException e) {
+			Alerts.showAlert("Erro salvando objeto", null, e.getMessage(), AlertType.ERROR);
+		}
 	}
-	
+
+	private Department getFormData() {
+
+		Department obj = new Department();
+		obj.setId(Utils.tryParseToInt(txtId.getText()));
+		obj.setName(txtName.getText());
+
+		return obj;
+	}
+
 	@FXML
-	private void onBtCancelAction() {
-		System.out.println("onBtCancelAction");
+	private void onBtCancelAction(ActionEvent event) {
+
+		Utils.currentStage(event).close();
+
 	}
-	
+
 	public void setDepartment(Department entity) {
 		this.entity = entity;
 	}
-	
+
+	public void setDepartmentService(DepartmentService service) {
+		this.service = service;
+	}
+
 	@Override
 	public void initialize(URL url, ResourceBundle rb) {
 		initializeNodes();
 	}
 
-	private void initializeNodes () {
+	private void initializeNodes() {
 		Constraints.setTextFieldInteger(txtId);
 		Constraints.setTextFieldMaxLength(txtName, 30);
 	}
-	
+
 	public void updateFormData() {
 		if (entity == null) {
 			throw new IllegalStateException("Entidade está nula");
 		}
-		
+
 		txtId.setText(String.valueOf(entity.getId()));
 		txtName.setText(entity.getName());
 	}
